844b84e7b52d119338792b5811e63513146921dd
Walter Medvedeo <wmedvedeo@gmail.com>
2016-02-02 19:15:41 +0100

    JBPM-4833 - Data Modeller MVP, Tests, and related improvments
        -DataObjectBrowser, MPV improvements + tests + i18n review
        -Field Injection Constructor and Missing scope added to DataModelerScreenViewImpl

--- kie-wb-common-screens/kie-wb-common-data-modeller/kie-wb-common-data-modeller-client/src/main/java/org/kie/workbench/common/screens/datamodeller/client/DataModelerScreenViewImpl.java
+++ kie-wb-common-screens/kie-wb-common-data-modeller/kie-wb-common-data-modeller-client/src/main/java/org/kie/workbench/common/screens/datamodeller/client/DataModelerScreenViewImpl.java
@@ -17,6 +17,7 @@
 package org.kie.workbench.common.screens.datamodeller.client;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -25,13 +26,13 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.RequiresResize;
 import com.google.gwt.user.client.ui.Widget;
 import org.gwtbootstrap3.client.ui.Legend;
 import org.kie.workbench.common.screens.datamodeller.client.context.DataModelerWorkbenchContext;
 import org.kie.workbench.common.screens.datamodeller.client.context.DataModelerWorkbenchContextChangeEvent;
+import org.kie.workbench.common.screens.datamodeller.client.resources.i18n.Constants;
 import org.kie.workbench.common.screens.datamodeller.client.util.DataModelerUtils;
-import org.kie.workbench.common.screens.datamodeller.client.widgets.DataObjectBrowser;
+import org.kie.workbench.common.screens.datamodeller.client.widgets.editor.DataObjectBrowser;
 import org.kie.workbench.common.screens.datamodeller.client.widgets.maindomain.MainDomainEditor;
 import org.kie.workbench.common.screens.datamodeller.events.DataModelStatusChangeEvent;
 import org.kie.workbench.common.screens.datamodeller.events.DataModelerEvent;
@@ -43,10 +44,10 @@ import org.kie.workbench.common.services.datamodeller.core.DataObject;
 import org.kie.workbench.common.services.datamodeller.core.ObjectProperty;
 import org.kie.workbench.common.widgets.metadata.client.KieEditorViewImpl;
 
-
+@Dependent
 public class DataModelerScreenViewImpl
         extends KieEditorViewImpl
-        implements DataModelerScreenPresenter.DataModelerScreenView, RequiresResize {
+        implements DataModelerScreenPresenter.DataModelerScreenView {
 
     interface DataModelerScreenViewBinder
             extends
@@ -65,22 +66,26 @@ public class DataModelerScreenViewImpl
     @UiField
     Legend domainContainerTitle;
 
-    @Inject
     private DataObjectBrowser dataObjectBrowser;
 
-    @Inject
     private MainDomainEditor mainDomainEditor;
 
-    @Inject
     private Event<DataModelerEvent> dataModelerEvent;
 
     private DataModelerContext context;
 
-    @Inject
     private DataModelerWorkbenchContext dataModelerWBContext;
 
-    public DataModelerScreenViewImpl() {
+    @Inject
+    public DataModelerScreenViewImpl( DataObjectBrowser dataObjectBrowser,
+            MainDomainEditor mainDomainEditor,
+            Event<DataModelerEvent> dataModelerEvent,
+            DataModelerWorkbenchContext dataModelerWBContext ) {
         initWidget( uiBinder.createAndBindUi( this ) );
+        this.dataObjectBrowser = dataObjectBrowser;
+        this.mainDomainEditor = mainDomainEditor;
+        this.dataModelerEvent = dataModelerEvent;
+        this.dataModelerWBContext = dataModelerWBContext;
     }
 
     @PostConstruct
@@ -110,7 +115,7 @@ public class DataModelerScreenViewImpl
     private void refreshTitle( DataObject dataObject ) {
         if ( dataObject != null ) {
             String label = DataModelerUtils.getDataObjectFullLabel( dataObject, false );
-            String title = "'" + label + "'" + " - general properties";
+            String title = "'" + label + "'" + Constants.INSTANCE.modelEditor_general_properties_label();
             String tooltip = dataObject.getClassName();
             domainContainerTitle.setText( title );
             domainContainerTitle.setTitle( tooltip );
@@ -119,7 +124,7 @@ public class DataModelerScreenViewImpl
 
     private void refreshTitle( DataObject dataObject, ObjectProperty objectProperty ) {
         if ( dataObject != null && objectProperty != null ) {
-            String title = "'" + objectProperty.getName() + "'" + " - general properties";
+            String title = "'" + objectProperty.getName() + "'" + Constants.INSTANCE.modelEditor_general_properties_label();
             String tooltip = dataObject.getClassName() + "." + objectProperty.getName();
             domainContainerTitle.setText( title );
             domainContainerTitle.setTitle( tooltip );
@@ -163,10 +168,4 @@ public class DataModelerScreenViewImpl
         updateChangeStatus( event );
     }
 
-    @Override
-    public void onResize() {
-        int height = getParent().getOffsetHeight();
-        int width = getParent().getOffsetWidth();
-        setPixelSize( width, height );
-    }
 }
