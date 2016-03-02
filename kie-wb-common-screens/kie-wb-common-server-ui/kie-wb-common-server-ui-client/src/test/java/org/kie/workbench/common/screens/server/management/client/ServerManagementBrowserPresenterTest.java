7cd038c0a9674e5ec0381ec4b681b962ff9dee26
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-03-02 20:14:48 +1000

    GUVNOR-2452: Enhanced tests for presenter classes

--- kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/test/java/org/kie/workbench/common/screens/server/management/client/ServerManagementBrowserPresenterTest.java
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/test/java/org/kie/workbench/common/screens/server/management/client/ServerManagementBrowserPresenterTest.java
@@ -29,6 +29,7 @@ import org.kie.server.controller.api.model.events.ServerInstanceDeleted;
 import org.kie.server.controller.api.model.events.ServerTemplateDeleted;
 import org.kie.server.controller.api.model.events.ServerTemplateUpdated;
 import org.kie.server.controller.api.model.runtime.ServerInstanceKey;
+import org.kie.server.controller.api.model.spec.ContainerSpec;
 import org.kie.server.controller.api.model.spec.ContainerSpecKey;
 import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.server.controller.api.model.spec.ServerTemplateKey;
@@ -88,9 +89,9 @@ public class ServerManagementBrowserPresenterTest {
 
     @Before
     public void init() {
-        specManagementServiceCaller = new CallerMock<SpecManagementService>(specManagementService);
-        doNothing().when(serverTemplateSelectedEvent).fire(any(ServerTemplateSelected.class));
-        presenter = spy(new ServerManagementBrowserPresenter(
+        specManagementServiceCaller = new CallerMock<SpecManagementService>( specManagementService );
+        doNothing().when( serverTemplateSelectedEvent ).fire( any( ServerTemplateSelected.class ) );
+        presenter = spy( new ServerManagementBrowserPresenter(
                 view,
                 navigationPresenter,
                 serverTemplatePresenter,
@@ -99,127 +100,153 @@ public class ServerManagementBrowserPresenterTest {
                 containerPresenter,
                 remotePresenter,
                 specManagementServiceCaller,
-                serverTemplateSelectedEvent));
+                serverTemplateSelectedEvent ) );
     }
 
     @Test
     public void testInit() {
         presenter.init();
 
-        verify(view).setNavigation(navigationPresenter.getView());
-        assertEquals(view, presenter.getView());
+        verify( view ).setNavigation( navigationPresenter.getView() );
+        assertEquals( view, presenter.getView() );
     }
 
     @Test
     public void testOnSelectedContainerSpec() {
-        final ContainerPresenter.View containerView = mock(ContainerPresenter.View.class);
-        when(containerPresenter.getView()).thenReturn(containerView);
+        final ContainerPresenter.View containerView = mock( ContainerPresenter.View.class );
+        when( containerPresenter.getView() ).thenReturn( containerView );
 
-        presenter.onSelected(new ContainerSpecSelected(new ContainerSpecKey()));
+        presenter.onSelected( new ContainerSpecSelected( new ContainerSpecKey() ) );
 
-        verify(view).setContent(containerView);
+        verify( view ).setContent( containerView );
     }
 
     @Test
     public void testOnSelectedServerInstance() {
-        final RemotePresenter.View remoteView = mock(RemotePresenter.View.class);
-        when(remotePresenter.getView()).thenReturn(remoteView);
+        final RemotePresenter.View remoteView = mock( RemotePresenter.View.class );
+        when( remotePresenter.getView() ).thenReturn( remoteView );
 
-        presenter.onSelected(new ServerInstanceSelected(new ServerInstanceKey()));
+        presenter.onSelected( new ServerInstanceSelected( new ServerInstanceKey() ) );
 
-        verify(view).setContent(remoteView);
+        verify( view ).setContent( remoteView );
     }
 
     @Test
     public void testOnSelectedServerTemplate() {
-        final ServerTemplate serverTemplate = new ServerTemplate("ServerTemplateId", "ServerTemplateName");
-        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey("ServerTemplateKeyId", "ServerTemplateKeyName");
-        when(specManagementService.getServerTemplate(serverTemplateKey.getId())).thenReturn(serverTemplate);
-        final ServerTemplatePresenter.View serverView = mock(ServerTemplatePresenter.View.class);
-        when(serverTemplatePresenter.getView()).thenReturn(serverView);
-        final ServerContainerEmptyPresenter.View serverEmptyView = mock(ServerContainerEmptyPresenter.View.class);
-        when(serverContainerEmptyPresenter.getView()).thenReturn(serverEmptyView);
+        final ServerTemplate serverTemplate = new ServerTemplate( "ServerTemplateId", "ServerTemplateName" );
+        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey( "ServerTemplateKeyId", "ServerTemplateKeyName" );
+        when( specManagementService.getServerTemplate( serverTemplateKey.getId() ) ).thenReturn( serverTemplate );
+        final ServerTemplatePresenter.View serverView = mock( ServerTemplatePresenter.View.class );
+        when( serverTemplatePresenter.getView() ).thenReturn( serverView );
+        final ServerContainerEmptyPresenter.View serverEmptyView = mock( ServerContainerEmptyPresenter.View.class );
+        when( serverContainerEmptyPresenter.getView() ).thenReturn( serverEmptyView );
 
-        presenter.onSelected(new ServerTemplateSelected(serverTemplateKey));
+        presenter.onSelected( new ServerTemplateSelected( serverTemplateKey ) );
 
-        verify(view).setServerTemplate(serverView);
-        verify(specManagementService).getServerTemplate(serverTemplateKey.getId());
-        verify(serverContainerEmptyPresenter).setTemplate(serverTemplate);
-        verify(view).setContent(serverEmptyView);
-        verify(serverTemplatePresenter).setup(serverTemplate, null);
+        verify( view ).setServerTemplate( serverView );
+        verify( specManagementService ).getServerTemplate( serverTemplateKey.getId() );
+        verify( serverContainerEmptyPresenter ).setTemplate( serverTemplate );
+        verify( view ).setContent( serverEmptyView );
+        verify( serverTemplatePresenter ).setup( serverTemplate, null );
+    }
+
+    @Test
+    public void testOnSelectedNonEmptyServerTemplate() {
+        final ServerTemplate serverTemplate = new ServerTemplate( "ServerTemplateId", "ServerTemplateName" );
+        final ContainerSpec toBeSelected = mock( ContainerSpec.class );
+        serverTemplate.addContainerSpec( toBeSelected );
+        when( toBeSelected.getId() ).thenReturn( "other-id" );
+        final ContainerSpec forcedToBeSelected = mock( ContainerSpec.class );
+        when( forcedToBeSelected.getId() ).thenReturn( "container-id" );
+        serverTemplate.addContainerSpec( forcedToBeSelected );
+
+        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey( "ServerTemplateKeyId", "ServerTemplateKeyName" );
+        when( specManagementService.getServerTemplate( serverTemplateKey.getId() ) ).thenReturn( serverTemplate );
+        final ServerTemplatePresenter.View serverView = mock( ServerTemplatePresenter.View.class );
+        when( serverTemplatePresenter.getView() ).thenReturn( serverView );
+
+        presenter.onSelected( new ServerTemplateSelected( serverTemplateKey ) );
+
+        verify( view ).setServerTemplate( serverView );
+        verify( specManagementService ).getServerTemplate( serverTemplateKey.getId() );
+        verify( serverTemplatePresenter ).setup( serverTemplate, toBeSelected );
+
+        presenter.onSelected( new ServerTemplateSelected( serverTemplateKey, "container-id" ) );
+
+        verify( serverTemplatePresenter ).setup( serverTemplate, forcedToBeSelected );
     }
 
     @Test
     public void testOnOpen() {
-        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey("ServerTemplateKeyId", "ServerTemplateKeyName");
-        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList(serverTemplateKey);
-        when(specManagementService.listServerTemplateKeys()).thenReturn(serverTemplateKeys);
+        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey( "ServerTemplateKeyId", "ServerTemplateKeyName" );
+        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList( serverTemplateKey );
+        when( specManagementService.listServerTemplateKeys() ).thenReturn( serverTemplateKeys );
 
         presenter.onOpen();
 
-        verify(navigationPresenter).setup(serverTemplateKey, serverTemplateKeys);
-        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass(ServerTemplateSelected.class);
-        verify(serverTemplateSelectedEvent).fire(templateSelectedCaptor.capture());
-        assertEquals(serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey());
+        verify( navigationPresenter ).setup( serverTemplateKey, serverTemplateKeys );
+        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass( ServerTemplateSelected.class );
+        verify( serverTemplateSelectedEvent ).fire( templateSelectedCaptor.capture() );
+        assertEquals( serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey() );
     }
 
     @Test
     public void testSetupEmpty() {
-        final ServerEmptyPresenter.View serverEmptyView = mock(ServerEmptyPresenter.View.class);
-        when(serverEmptyPresenter.getView()).thenReturn(serverEmptyView);
+        final ServerEmptyPresenter.View serverEmptyView = mock( ServerEmptyPresenter.View.class );
+        when( serverEmptyPresenter.getView() ).thenReturn( serverEmptyView );
 
-        presenter.setup(Collections.<ServerTemplateKey>emptyList(), null);
+        presenter.setup( Collections.<ServerTemplateKey>emptyList(), null );
 
-        verify(view).setEmptyView(serverEmptyView);
-        verify(navigationPresenter).clear();
+        verify( view ).setEmptyView( serverEmptyView );
+        verify( navigationPresenter ).clear();
     }
 
     @Test
     public void testOnServerDeleted() {
-        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey("ServerTemplateKeyId", "ServerTemplateKeyName");
-        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList(serverTemplateKey);
-        when(specManagementService.listServerTemplateKeys()).thenReturn(serverTemplateKeys);
+        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey( "ServerTemplateKeyId", "ServerTemplateKeyName" );
+        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList( serverTemplateKey );
+        when( specManagementService.listServerTemplateKeys() ).thenReturn( serverTemplateKeys );
 
-        presenter.onServerDeleted(new ServerTemplateDeleted());
+        presenter.onServerDeleted( new ServerTemplateDeleted() );
 
-        verify(navigationPresenter).setup(serverTemplateKey, serverTemplateKeys);
-        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass(ServerTemplateSelected.class);
-        verify(serverTemplateSelectedEvent).fire(templateSelectedCaptor.capture());
-        assertEquals(serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey());
+        verify( navigationPresenter ).setup( serverTemplateKey, serverTemplateKeys );
+        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass( ServerTemplateSelected.class );
+        verify( serverTemplateSelectedEvent ).fire( templateSelectedCaptor.capture() );
+        assertEquals( serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey() );
     }
 
     @Test
     public void testOnServerTemplateUpdated() {
-        final ServerTemplate serverTemplate = new ServerTemplate("ServerTemplateId", "ServerTemplateName");
+        final ServerTemplate serverTemplate = new ServerTemplate( "ServerTemplateId", "ServerTemplateName" );
 
-        presenter.onServerTemplateUpdated(new ServerTemplateUpdated(serverTemplate));
+        presenter.onServerTemplateUpdated( new ServerTemplateUpdated( serverTemplate ) );
 
-        final ArgumentCaptor<Collection> serverTemplateKeysCaptor = ArgumentCaptor.forClass(Collection.class);
-        verify(navigationPresenter).setup(eq(serverTemplate), serverTemplateKeysCaptor.capture());
+        final ArgumentCaptor<Collection> serverTemplateKeysCaptor = ArgumentCaptor.forClass( Collection.class );
+        verify( navigationPresenter ).setup( eq( serverTemplate ), serverTemplateKeysCaptor.capture() );
         final Collection<ServerTemplateKey> serverTemplateKeys = serverTemplateKeysCaptor.getValue();
-        assertEquals(1, serverTemplateKeys.size());
-        assertTrue(serverTemplateKeys.contains(serverTemplate));
+        assertEquals( 1, serverTemplateKeys.size() );
+        assertTrue( serverTemplateKeys.contains( serverTemplate ) );
 
-        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass(ServerTemplateSelected.class);
-        verify(serverTemplateSelectedEvent).fire(templateSelectedCaptor.capture());
-        assertEquals(serverTemplate, templateSelectedCaptor.getValue().getServerTemplateKey());
+        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass( ServerTemplateSelected.class );
+        verify( serverTemplateSelectedEvent ).fire( templateSelectedCaptor.capture() );
+        assertEquals( serverTemplate, templateSelectedCaptor.getValue().getServerTemplateKey() );
     }
 
     @Test
     public void testOnDelete() {
-        final ServerInstanceKey serverInstanceKey = new ServerInstanceKey("serverInstanceKeyId", "serverName", "serverInstanceId", "url");
-        final ServerTemplate serverTemplate = new ServerTemplate("ServerTemplateId", "ServerTemplateName");
-        serverTemplate.addServerInstance(serverInstanceKey);
-        when(serverTemplatePresenter.getCurrentServerTemplate()).thenReturn(serverTemplate);
-        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey("ServerTemplateKeyId", "ServerTemplateKeyName");
-        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList(serverTemplateKey);
-        when(specManagementService.listServerTemplateKeys()).thenReturn(serverTemplateKeys);
+        final ServerInstanceKey serverInstanceKey = new ServerInstanceKey( "serverInstanceKeyId", "serverName", "serverInstanceId", "url" );
+        final ServerTemplate serverTemplate = new ServerTemplate( "ServerTemplateId", "ServerTemplateName" );
+        serverTemplate.addServerInstance( serverInstanceKey );
+        when( serverTemplatePresenter.getCurrentServerTemplate() ).thenReturn( serverTemplate );
+        final ServerTemplateKey serverTemplateKey = new ServerTemplateKey( "ServerTemplateKeyId", "ServerTemplateKeyName" );
+        final List<ServerTemplateKey> serverTemplateKeys = Collections.singletonList( serverTemplateKey );
+        when( specManagementService.listServerTemplateKeys() ).thenReturn( serverTemplateKeys );
 
-        presenter.onDelete(new ServerInstanceDeleted(serverInstanceKey.getServerInstanceId()));
+        presenter.onDelete( new ServerInstanceDeleted( serverInstanceKey.getServerInstanceId() ) );
 
-        verify(navigationPresenter).setup(serverTemplateKey, serverTemplateKeys);
-        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass(ServerTemplateSelected.class);
-        verify(serverTemplateSelectedEvent).fire(templateSelectedCaptor.capture());
-        assertEquals(serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey());
+        verify( navigationPresenter ).setup( serverTemplateKey, serverTemplateKeys );
+        final ArgumentCaptor<ServerTemplateSelected> templateSelectedCaptor = ArgumentCaptor.forClass( ServerTemplateSelected.class );
+        verify( serverTemplateSelectedEvent ).fire( templateSelectedCaptor.capture() );
+        assertEquals( serverTemplateKey, templateSelectedCaptor.getValue().getServerTemplateKey() );
     }
 }
