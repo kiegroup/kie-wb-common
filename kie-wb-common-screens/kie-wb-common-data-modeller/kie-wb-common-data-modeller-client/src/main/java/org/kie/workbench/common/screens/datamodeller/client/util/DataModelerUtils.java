c84fcecac79b0c1b474788748171f64116a763cc
Paulo Martins <paulovmr@gmail.com>
2016-05-02 12:02:48 -0300

    BZ-1328663: selection list of fact fields are ordered randomly (not dictionary order) in GDT and test scenario editor

--- kie-wb-common-screens/kie-wb-common-data-modeller/kie-wb-common-data-modeller-client/src/main/java/org/kie/workbench/common/screens/datamodeller/client/util/DataModelerUtils.java
+++ kie-wb-common-screens/kie-wb-common-data-modeller/kie-wb-common-data-modeller-client/src/main/java/org/kie/workbench/common/screens/datamodeller/client/util/DataModelerUtils.java
@@ -30,6 +30,7 @@ import org.kie.workbench.common.services.datamodeller.core.DataModel;
 import org.kie.workbench.common.services.datamodeller.core.DataObject;
 import org.kie.workbench.common.services.datamodeller.core.ObjectProperty;
 import org.kie.workbench.common.services.datamodeller.core.PropertyType;
+import org.kie.workbench.common.services.datamodel.util.SortHelper;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.commons.data.Pair;
 
@@ -301,10 +302,10 @@ public class DataModelerUtils {
 
         List<Pair<String, String>> typeList = new ArrayList<Pair<String, String>>( );
 
-        SortedMap<String, String> sortedModelTypeNames = new TreeMap<String, String>();
-        SortedMap<String, String> sortedExternalTypeNames = new TreeMap<String, String>();
-        Map<String, PropertyType> orderedBaseTypes = new TreeMap<String, PropertyType>();
-        Map<String, PropertyType> baseTypesByClassName = new TreeMap<String, PropertyType>();
+        SortedMap<String, String> sortedModelTypeNames = new TreeMap<String, String>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
+        SortedMap<String, String> sortedExternalTypeNames = new TreeMap<String, String>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
+        Map<String, PropertyType> orderedBaseTypes = new TreeMap<String, PropertyType>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
+        Map<String, PropertyType> baseTypesByClassName = new TreeMap<String, PropertyType>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
         boolean selectedTypeIncluded = false;
 
         if ( includeEmptyItem ) {
@@ -382,8 +383,8 @@ public class DataModelerUtils {
         List<Pair<String, String>> options = new ArrayList<Pair<String, String>>();
 
         if ( dataModel != null ) {
-            SortedMap<String, String> sortedModelClasses = new TreeMap<String, String>();
-            SortedMap<String, String> sortedExternalClasses = new TreeMap<String, String>();
+            SortedMap<String, String> sortedModelClasses = new TreeMap<String, String>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
+            SortedMap<String, String> sortedExternalClasses = new TreeMap<String, String>( SortHelper.ALPHABETICAL_ORDER_COMPARATOR );
             boolean isExtensible = false;
             String className;
             String classLabel;
