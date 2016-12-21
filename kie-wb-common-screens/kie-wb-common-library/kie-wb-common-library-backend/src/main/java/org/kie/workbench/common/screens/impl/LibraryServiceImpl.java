652cbcfee8959dbd87b275008e9c690801158ad1
Paulo Martins <paulovmr@gmail.com>
2016-12-21 09:52:50 -0200

    GUVNOR-2812: Authoring mode: When using Library new project menu item is disabled

--- kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-backend/src/main/java/org/kie/workbench/common/screens/impl/LibraryServiceImpl.java
+++ kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-backend/src/main/java/org/kie/workbench/common/screens/impl/LibraryServiceImpl.java
@@ -211,7 +211,6 @@ public class LibraryServiceImpl implements LibraryService {
 
     RepositoryEnvironmentConfigurations getDefaultRepositoryEnvironmentConfigurations() {
         final RepositoryEnvironmentConfigurations configuration = new RepositoryEnvironmentConfigurations();
-        configuration.setManaged( true );
         return configuration;
     }
 
