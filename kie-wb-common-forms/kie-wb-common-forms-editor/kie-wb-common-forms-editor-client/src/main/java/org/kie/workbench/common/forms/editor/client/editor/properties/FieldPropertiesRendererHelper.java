884bc97add554f80fcd8531d00b2c1478a8278bc
Pere Fernandez <pefernan@users.noreply.github.com>
2017-08-29 17:36:30 +0200

    JBPM-6324: Inconsistency between field types and field bindings with slider.

--- kie-wb-common-forms/kie-wb-common-forms-editor/kie-wb-common-forms-editor-client/src/main/java/org/kie/workbench/common/forms/editor/client/editor/properties/FieldPropertiesRendererHelper.java
+++ kie-wb-common-forms/kie-wb-common-forms-editor/kie-wb-common-forms-editor-client/src/main/java/org/kie/workbench/common/forms/editor/client/editor/properties/FieldPropertiesRendererHelper.java
@@ -29,7 +29,7 @@ public interface FieldPropertiesRendererHelper {
 
     FieldDefinition getCurrentField();
 
-    List<String> getAvailableModelFields();
+    List<String> getAvailableModelFields(FieldDefinition fieldDefinition);
 
     List<String> getCompatibleFieldTypes(FieldDefinition fieldDefinition);
 
