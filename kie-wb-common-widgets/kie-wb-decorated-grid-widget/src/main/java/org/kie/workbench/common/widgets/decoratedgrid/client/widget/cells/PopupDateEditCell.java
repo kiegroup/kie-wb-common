5277c944e76a31227c40f1ddbdb7420849cf2f61
Michael Anstis <manstis@users.noreply.github.com>
2017-09-20 08:19:56 +0100

    RHBPMS-4856: [GSS](6.4.z) Calendar is not visible on screen if guided decision table data grows BPM Suite 6.4 (#1171)

--- kie-wb-common-widgets/kie-wb-decorated-grid-widget/src/main/java/org/kie/workbench/common/widgets/decoratedgrid/client/widget/cells/PopupDateEditCell.java
+++ kie-wb-common-widgets/kie-wb-decorated-grid-widget/src/main/java/org/kie/workbench/common/widgets/decoratedgrid/client/widget/cells/PopupDateEditCell.java
@@ -17,12 +17,18 @@ package org.kie.workbench.common.widgets.decoratedgrid.client.widget.cells;
 
 import java.util.Date;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.PopupPanel.PositionCallback;
+import org.gwtbootstrap3.client.shared.event.HideEvent;
+import org.gwtbootstrap3.client.shared.event.HideHandler;
+import org.gwtbootstrap3.client.shared.event.ShowEvent;
+import org.gwtbootstrap3.client.shared.event.ShowHandler;
+import org.gwtbootstrap3.extras.datepicker.client.ui.base.constants.DatePickerPosition;
 import org.uberfire.ext.widgets.common.client.common.DatePicker;
 
 /**
@@ -33,47 +39,60 @@ public class PopupDateEditCell extends AbstractPopupEditCell<Date, Date> {
     private final DatePicker datePicker;
     private final DateTimeFormat format;
 
-    public PopupDateEditCell( DateTimeFormat format,
-                              boolean isReadOnly ) {
-        super( isReadOnly );
-        if ( format == null ) {
-            throw new IllegalArgumentException( "format == null" );
+    public PopupDateEditCell(DateTimeFormat format,
+                             boolean isReadOnly) {
+        super(isReadOnly);
+        if (format == null) {
+            throw new IllegalArgumentException("format == null");
         }
 
         this.format = format;
-        this.datePicker = new DatePicker();
-        datePicker.setFormat( format.getPattern() );
+        this.datePicker = GWT.create(DatePicker.class);
+        datePicker.setFormat(format.getPattern());
+        datePicker.setPosition(DatePickerPosition.AUTO);
 
         // See https://issues.jboss.org/browse/GUVNOR-2322
-        // The DatePicker was being closed, before the ValueChangeHandler invoked, in response to the
-        // containing PopupPanel being automatically hidden when another Element received events.
-        datePicker.setContainer( vPanel );
-        panel.addAutoHidePartner( datePicker.getElement() );
+        // Register the DatePicker DOM components as "auto hide partners"
+        // to ensure click events on them do not close the PopupPanel.
+        panel.addAutoHidePartner(datePicker.getElement());
+        datePicker.addShowHandler(new ShowHandler() {
+            @Override
+            public void onShow(final ShowEvent showEvent) {
+                addDatePickerAsAutoHidePartner();
+            }
+        });
+        datePicker.addHideHandler(new HideHandler() {
+            @Override
+            public void onHide(final HideEvent hideEvent) {
+                removeDatePickerAsAutoHidePartner();
+            }
+        });
 
         // Hide the panel and call valueUpdater.update when a date is selected
-        datePicker.addValueChangeHandler( new ValueChangeHandler<Date>() {
+        datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
             @Override
-            public void onValueChange( final ValueChangeEvent<Date> event ) {
-                Date date = datePicker.getValue();
-                setValue( lastContext,
-                          lastParent,
-                          date );
-                if ( valueUpdater != null ) {
-                    valueUpdater.update( date );
-                }
-                panel.hide();
+            public void onValueChange(final ValueChangeEvent<Date> event) {
+                commit();
             }
-        } );
+        });
 
-        vPanel.add( datePicker );
+        vPanel.add(datePicker);
+    }
+
+    void addDatePickerAsAutoHidePartner() {
+        panel.addAutoHidePartner(panel.getElement().getNextSiblingElement());
+    }
+
+    void removeDatePickerAsAutoHidePartner() {
+        panel.removeAutoHidePartner(panel.getElement().getNextSiblingElement());
     }
 
     @Override
-    public void render( Context context,
-                        Date value,
-                        SafeHtmlBuilder sb ) {
-        if ( value != null ) {
-            sb.append( renderer.render( format.format( value ) ) );
+    public void render(Context context,
+                       Date value,
+                       SafeHtmlBuilder sb) {
+        if (value != null) {
+            sb.append(renderer.render(format.format(value)));
         }
     }
 
@@ -83,11 +102,11 @@ public class PopupDateEditCell extends AbstractPopupEditCell<Date, Date> {
 
         // Update value
         Date date = datePicker.getValue();
-        setValue( lastContext,
-                  lastParent,
-                  date );
-        if ( valueUpdater != null ) {
-            valueUpdater.update( date );
+        setValue(lastContext,
+                 lastParent,
+                 date);
+        if (valueUpdater != null) {
+            valueUpdater.update(date);
         }
         panel.hide();
     }
@@ -95,33 +114,31 @@ public class PopupDateEditCell extends AbstractPopupEditCell<Date, Date> {
     // Start editing the cell
     @Override
     @SuppressWarnings("deprecation")
-    protected void startEditing( final Context context,
-                                 final Element parent,
-                                 final Date value ) {
+    protected void startEditing(final Context context,
+                                final Element parent,
+                                final Date value) {
 
         // Default date
         Date date = value;
-        if ( value == null ) {
+        if (value == null) {
             Date d = new Date();
             int year = d.getYear();
             int month = d.getMonth();
             int dom = d.getDate();
-            date = new Date( year,
-                             month,
-                             dom );
+            date = new Date(year,
+                            month,
+                            dom);
         }
-        datePicker.setValue( date );
+        datePicker.setValue(date);
 
-        panel.setPopupPositionAndShow( new PositionCallback() {
-            public void setPosition( int offsetWidth,
-                                     int offsetHeight ) {
-                panel.setPopupPosition( parent.getAbsoluteLeft()
-                                                + offsetX,
-                                        parent.getAbsoluteTop()
-                                                + offsetY );
+        panel.setPopupPositionAndShow(new PositionCallback() {
+            public void setPosition(int offsetWidth,
+                                    int offsetHeight) {
+                panel.setPopupPosition(parent.getAbsoluteLeft()
+                                               + offsetX,
+                                       parent.getAbsoluteTop()
+                                               + offsetY);
             }
-        } );
-
+        });
     }
-
 }
