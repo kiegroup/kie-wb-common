2c3a8720ae72cd81f66e051912a5804fe98cd3db
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2017-10-18 12:09:21 -0200

    Use XStreamUtils.createTrustingXStream(..) helper

--- kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/test/java/org/kie/workbench/common/screens/server/management/backend/storage/ServerTemplateMigrationTest.java
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/test/java/org/kie/workbench/common/screens/server/management/backend/storage/ServerTemplateMigrationTest.java
@@ -27,13 +27,11 @@ import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kie.internal.xstream.XStreamUtils;
 import org.kie.server.api.model.KieContainerStatus;
 import org.kie.server.api.model.ReleaseId;
 import org.kie.server.controller.api.model.runtime.ServerInstanceKey;
-import org.kie.server.controller.api.model.spec.Capability;
 import org.kie.server.controller.api.model.spec.ContainerSpec;
-import org.kie.server.controller.api.model.spec.ProcessConfig;
-import org.kie.server.controller.api.model.spec.RuleConfig;
 import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.workbench.common.screens.server.management.backend.storage.migration.ServerTemplateMigration;
 import org.uberfire.io.IOService;
@@ -54,9 +52,7 @@ public class ServerTemplateMigrationTest {
 
     @Before
     public void setup() throws IOException {
-        String[] voidDeny = {"void.class", "Void.class"};
-        xstream = new XStream();
-        xstream.denyTypes(voidDeny);
+        xstream = XStreamUtils.createTrustingXStream();
 
         fileSystemTestingUtils.setup();
 
