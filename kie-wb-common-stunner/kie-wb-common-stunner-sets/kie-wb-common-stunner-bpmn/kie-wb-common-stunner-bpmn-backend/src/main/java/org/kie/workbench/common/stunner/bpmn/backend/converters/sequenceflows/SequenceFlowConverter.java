7bb228cc8f369148de137924c052fc663bd981e7
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-01-29 15:46:12 +0100

    sequence flow: use new style

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/sequenceflows/SequenceFlowConverter.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/sequenceflows/SequenceFlowConverter.java
@@ -17,13 +17,17 @@
 package org.kie.workbench.common.stunner.bpmn.backend.converters.sequenceflows;
 
 import org.eclipse.bpmn2.FormalExpression;
-import org.eclipse.emf.ecore.util.FeatureMap;
 import org.kie.workbench.common.stunner.bpmn.backend.converters.GraphBuildingContext;
 import org.kie.workbench.common.stunner.bpmn.backend.converters.TypedFactoryManager;
 import org.kie.workbench.common.stunner.bpmn.backend.converters.properties.Properties;
 import org.kie.workbench.common.stunner.bpmn.backend.converters.properties.ScriptLanguages;
 import org.kie.workbench.common.stunner.bpmn.definition.SequenceFlow;
+import org.kie.workbench.common.stunner.bpmn.definition.property.connectors.ConditionExpression;
+import org.kie.workbench.common.stunner.bpmn.definition.property.connectors.ConditionExpressionLanguage;
+import org.kie.workbench.common.stunner.bpmn.definition.property.connectors.Priority;
 import org.kie.workbench.common.stunner.bpmn.definition.property.connectors.SequenceFlowExecutionSet;
+import org.kie.workbench.common.stunner.bpmn.definition.property.general.BPMNGeneralSet;
+import org.kie.workbench.common.stunner.bpmn.definition.property.general.Name;
 import org.kie.workbench.common.stunner.core.graph.Edge;
 import org.kie.workbench.common.stunner.core.graph.Node;
 import org.kie.workbench.common.stunner.core.graph.content.view.View;
@@ -43,26 +47,38 @@ public class SequenceFlowConverter {
 
     public Edge<? extends View<SequenceFlow>, ?> convert(org.eclipse.bpmn2.SequenceFlow seq) {
         Edge<View<SequenceFlow>, Node> edge = factoryManager.newEdge(seq.getId(), SequenceFlow.class);
+
         SequenceFlow definition = edge.getContent().getDefinition();
-        definition.getGeneral().getName().setValue(seq.getName());
+
+        definition.setGeneral(new BPMNGeneralSet(
+                new Name(seq.getName()),
+                Properties.documentation(seq.getDocumentation())
+        ));
+
         FormalExpression conditionExpression = (FormalExpression) seq.getConditionExpression();
-        SequenceFlowExecutionSet executionSet = definition.getExecutionSet();
-        if (conditionExpression != null) {
-            String body = conditionExpression.getBody();
-            executionSet.getConditionExpression().setValue(body);
+
+        if (conditionExpression == null) {
+            definition.setExecutionSet(new SequenceFlowExecutionSet(
+                    new Priority(Properties.findAnyAttribute(seq, "priority")),
+                    new ConditionExpression(),
+                    new ConditionExpressionLanguage()
+            ));
+        } else {
             String language = conditionExpression.getLanguage();
-            executionSet.getConditionExpressionLanguage().setValue(ScriptLanguages.fromUri(language));
-        }
-        for (FeatureMap.Entry entry : seq.getAnyAttribute()) {
-            if (entry.getEStructuralFeature().getName().equals("priority")) {
-                executionSet.getPriority().setValue(entry.getValue().toString());
-            }
+
+            definition.setExecutionSet(new SequenceFlowExecutionSet(
+                    new Priority(Properties.findAnyAttribute(seq, "priority")),
+                    new ConditionExpression(conditionExpression.getBody()),
+                    new ConditionExpressionLanguage(ScriptLanguages.fromUri(language))
+            ));
         }
 
         String sourceId = seq.getSourceRef().getId();
         String targetId = seq.getTargetRef().getId();
+
         boolean isAutoConnectionSource = Properties.findMetaBoolean(seq, "isAutoConnection.source");
         boolean isAutoConnectionTarget = Properties.findMetaBoolean(seq, "isAutoConnection.target");
+
         context.addEdge(
                 edge,
                 sourceId,
