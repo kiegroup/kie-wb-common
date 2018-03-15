f7d89d4f55e020476962f4ac7c6864ce4ff82425
Kirill Gaevskii <kgaevski@redhat.com>
2018-03-15 15:21:44 +0100

    Start Signal Events tests

---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/Marshalling/events/StartEvent.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.events;
+
+import org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.BPMNDiagramMarshallerBase;
+import org.kie.workbench.common.stunner.bpmn.definition.BaseStartEvent;
+import org.kie.workbench.common.stunner.bpmn.definition.property.dataio.AssignmentsInfo;
+import org.kie.workbench.common.stunner.bpmn.definition.property.dataio.DataIOSet;
+import org.kie.workbench.common.stunner.bpmn.definition.property.general.BPMNGeneralSet;
+import org.kie.workbench.common.stunner.core.diagram.Diagram;
+import org.kie.workbench.common.stunner.core.diagram.Metadata;
+import org.kie.workbench.common.stunner.core.graph.Graph;
+import org.kie.workbench.common.stunner.core.graph.Node;
+import org.kie.workbench.common.stunner.core.graph.content.definition.Definition;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public abstract class StartEvent extends BPMNDiagramMarshallerBase {
+
+    protected static final String EMPTY_VALUE = "";
+    protected static final boolean NON_INTERRUPTING = false;
+    protected static final boolean INTERRUPTING = true;
+
+    protected <T extends BaseStartEvent> void assertNodesEqualsAfterMarshalling(Diagram<Graph, Metadata> before, Diagram<Graph, Metadata> after, String nodeId, Class<T> startType) {
+        T nodeBeforeMarshalling = getStartNodeById(before, nodeId, startType);
+        T nodeAfterMarshalling = getStartNodeById(after, nodeId, startType);
+        assertEquals(nodeBeforeMarshalling, nodeAfterMarshalling);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected  <T extends BaseStartEvent> T getStartNodeById(Diagram<Graph, Metadata> diagram, String id, Class<T> type) {
+        Node<? extends Definition, ?> node = diagram.getGraph().getNode(id);
+        assertNotNull(node);
+        assertEquals(1, node.getOutEdges().size());
+        return type.cast(node.getContent().getDefinition());
+    }
+
+    protected void assertGeneralSet(BPMNGeneralSet generalSet, String nodeName, String documentation) {
+        assertNotNull(generalSet);
+        assertNotNull(generalSet.getName());
+        assertNotNull(generalSet.getDocumentation());
+        assertEquals(nodeName, generalSet.getName().getValue());
+        assertEquals(documentation, generalSet.getDocumentation().getValue());
+    }
+
+
+    protected void assertDataIOSet(DataIOSet dataIOSet, String value) {
+        assertNotNull(dataIOSet);
+        AssignmentsInfo assignmentsInfo = dataIOSet.getAssignmentsinfo();
+        assertNotNull(assignmentsInfo);
+        assertEquals(value, assignmentsInfo.getValue());
+    }
+
+}
---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/Marshalling/events/StartSignalEventTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.events;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.Marshaller;
+import org.kie.workbench.common.stunner.bpmn.definition.StartSignalEvent;
+import org.kie.workbench.common.stunner.bpmn.definition.property.event.signal.InterruptingSignalEventExecutionSet;
+import org.kie.workbench.common.stunner.core.definition.service.DiagramMarshaller;
+import org.kie.workbench.common.stunner.core.diagram.Diagram;
+import org.kie.workbench.common.stunner.core.diagram.Metadata;
+import org.kie.workbench.common.stunner.core.graph.Graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.Marshaller.NEW;
+import static org.kie.workbench.common.stunner.bpmn.backend.service.diagram.Marshalling.Marshaller.OLD;
+
+@RunWith(Parameterized.class)
+public class StartSignalEventTest extends StartEvent {
+
+    private static final String BPMN_START_SIGNAL_EVENT = "org/kie/workbench/common/stunner/bpmn/backend/service/diagram/signalStartEvents.bpmn";
+
+    private static final String FILLED_TOP_LEVEL_EVENT_ID = "40270ECD-79FB-4211-BBF5-B1B6DF39CC24";
+    private static final String EMPTY_TOP_LEVEL_EVENT_ID = "6DF9C3AB-7A3B-44D0-AAE1-C0E09D60ACC6";
+    private static final String FILLED_SUBPROCESS_LEVEL_EVENT_ID = "EEC78B63-5E63-4301-9B7F-30A26634091C";
+    private static final String EMPTY_SUBPROCESS_LEVEL_EVENT_ID = "E49AC940-F618-4F25-AE18-74AFDC923A7C";
+
+    private static final int AMOUNT_OF_NODES_IN_DIAGRAM = 11;
+
+    @Parameterized.Parameters
+    public static List<Object[]> marshallers() {
+        return Arrays.asList(new Object[][] {
+                {OLD}, {NEW}
+        });
+    }
+
+    private DiagramMarshaller<Graph, Metadata, Diagram<Graph, Metadata>> marshaller = null;
+
+    public StartSignalEventTest(Marshaller marshallerType) {
+        switch(marshallerType) {
+            case OLD: marshaller = oldMarshaller; break;
+            case NEW: marshaller = newMarshaller; break;
+        }
+    }
+
+    @Test
+    public void testUnmarshallTopLevelEventFilledProperties() throws Exception {
+        final String signalName = "Signal Start Event with Name";
+        final String signalDocumentation = "Non empty\nDocumentation\n~`!@#$%^&*()_+=-{}|\\][:\";'?><,./";
+        final String signalRef = "Signal1";
+        final String signalDataOutput = "||event:String||[dout]event->processVar";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartSignalEvent filledTop = getStartNodeById(diagram, FILLED_TOP_LEVEL_EVENT_ID, StartSignalEvent.class);
+        assertGeneralSet(filledTop.getGeneral(), signalName, signalDocumentation);
+        assertSignalEventExecutionSet(filledTop.getExecutionSet(), signalRef, INTERRUPTING);
+        assertDataIOSet(filledTop.getDataIOSet(), signalDataOutput);
+    }
+
+    @Test
+    public void testUnmarshallTopLevelEmptyEventProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartSignalEvent emptyTop = getStartNodeById(diagram, EMPTY_TOP_LEVEL_EVENT_ID, StartSignalEvent.class);
+        assertGeneralSet(emptyTop.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+        assertSignalEventExecutionSet(emptyTop.getExecutionSet(), EMPTY_VALUE, NON_INTERRUPTING);
+        assertDataIOSet(emptyTop.getDataIOSet(), EMPTY_VALUE);
+    }
+
+    @Test
+    public void testUnmarshallSubprocessLevelEventFilledProperties() throws Exception {
+        final String signalName = "Signal inside of Event sub-process";
+        final String signalDocumentation = "Non empty Signal Event\nDocumentation\n~`!@#$%^&*()_+=-{}|\\][:\";'?><,./";
+        final String signalRef = "AnotherSignal";
+        final String signalDataOutput = "||hello:String||[dout]hello->processVar";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartSignalEvent filledSubprocess = getStartNodeById(diagram, FILLED_SUBPROCESS_LEVEL_EVENT_ID, StartSignalEvent.class);
+        assertGeneralSet(filledSubprocess.getGeneral(), signalName, signalDocumentation);
+        assertSignalEventExecutionSet(filledSubprocess.getExecutionSet(), signalRef, INTERRUPTING);
+        assertDataIOSet(filledSubprocess.getDataIOSet(), signalDataOutput);
+    }
+
+    @Test
+    public void testUnmarshallSubprocessLevelEventEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartSignalEvent emptySubprocess = getStartNodeById(diagram, EMPTY_SUBPROCESS_LEVEL_EVENT_ID, StartSignalEvent.class);
+        assertGeneralSet(emptySubprocess.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+        assertSignalEventExecutionSet(emptySubprocess.getExecutionSet(), EMPTY_VALUE, NON_INTERRUPTING);
+        assertDataIOSet(emptySubprocess.getDataIOSet(), EMPTY_VALUE);
+    }
+
+    @Test
+    public void testMarshallTopLevelEventFilledProperties() throws Exception {
+        Diagram<Graph, Metadata> initialDiagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        String resultXml = marshaller.marshall(initialDiagram);
+
+        Diagram<Graph, Metadata> marshalledDiagram = unmarshall(marshaller,  getStream(resultXml));
+        assertDiagram(marshalledDiagram,AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        assertNodesEqualsAfterMarshalling(initialDiagram, marshalledDiagram, FILLED_TOP_LEVEL_EVENT_ID, StartSignalEvent.class);
+    }
+
+    @Test
+    public void testMarshallTopLevelEmptyEventProperties() throws Exception {
+        Diagram<Graph, Metadata> initialDiagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        String resultXml = marshaller.marshall(initialDiagram);
+
+        Diagram<Graph, Metadata> marshalledDiagram = unmarshall(marshaller,  getStream(resultXml));
+        assertDiagram(marshalledDiagram,AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        assertNodesEqualsAfterMarshalling(initialDiagram, marshalledDiagram, EMPTY_TOP_LEVEL_EVENT_ID, StartSignalEvent.class);
+    }
+
+    @Test
+    public void testMarshallSubprocessLevelEventFilledProperties() throws Exception {
+        Diagram<Graph, Metadata> initialDiagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        String resultXml = marshaller.marshall(initialDiagram);
+
+        Diagram<Graph, Metadata> marshalledDiagram = unmarshall(marshaller,  getStream(resultXml));
+        assertDiagram(marshalledDiagram,AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        assertNodesEqualsAfterMarshalling(initialDiagram, marshalledDiagram, FILLED_SUBPROCESS_LEVEL_EVENT_ID, StartSignalEvent.class);
+    }
+
+    @Test
+    public void testMarshallSubprocessLevelEventEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> initialDiagram = unmarshall(marshaller, BPMN_START_SIGNAL_EVENT);
+        String resultXml = marshaller.marshall(initialDiagram);
+
+        Diagram<Graph, Metadata> marshalledDiagram = unmarshall(marshaller,  getStream(resultXml));
+        assertDiagram(marshalledDiagram,AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        assertNodesEqualsAfterMarshalling(initialDiagram, marshalledDiagram, EMPTY_SUBPROCESS_LEVEL_EVENT_ID, StartSignalEvent.class);
+    }
+
+    private void assertSignalEventExecutionSet(InterruptingSignalEventExecutionSet executionSet, String signalName, boolean isInterrupting) {
+        assertNotNull(executionSet);
+        assertNotNull(executionSet.getSignalRef());
+        assertEquals(signalName, executionSet.getSignalRef().getValue());
+        assertEquals(isInterrupting, executionSet.getIsInterrupting().getValue());
+    }
+}
