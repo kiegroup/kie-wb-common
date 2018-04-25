08ca6edac5da3e291c485ff14a52b2d95efc46da
Michael Anstis <manstis@users.noreply.github.com>
2018-04-25 13:14:06 +0100

    RHPAM-540 (JBPM-7031): Stunner - Disable doing operation in view mode (#1654)
    * JBPM-7031: Stunner - Disable doing operation in view mode
    
    * JBPM-7031: Stunner - Disable doing operation in view mode. Fix tests.
    
    (cherry picked from commit 5666b305e964f55fce1cd5bac4af65d89d9932ec)

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-project-client/src/test/java/org/kie/workbench/common/dmn/project/client/editor/DMNDiagramEditorTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.project.client.editor;
+
+import java.util.logging.Level;
+
+import com.google.gwtmockito.GwtMockitoTestRunner;
+import org.junit.runner.RunWith;
+import org.kie.workbench.common.dmn.project.client.type.DMNDiagramResourceType;
+import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler;
+import org.kie.workbench.common.stunner.project.client.editor.AbstractProjectDiagramEditor;
+import org.kie.workbench.common.stunner.project.client.editor.AbstractProjectDiagramEditorTest;
+import org.mockito.Mock;
+import org.uberfire.mvp.PlaceRequest;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+@RunWith(GwtMockitoTestRunner.class)
+public class DMNDiagramEditorTest extends AbstractProjectDiagramEditorTest {
+
+    @Mock
+    private PlaceRequest currentPlace;
+
+    @Mock
+    private AbstractCanvasHandler canvasHandler;
+
+    private DMNDiagramEditor diagramEditor;
+
+    @Override
+    protected DMNDiagramResourceType mockResourceType() {
+        final DMNDiagramResourceType resourceType = mock(DMNDiagramResourceType.class);
+        when(resourceType.getSuffix()).thenReturn("dmn");
+        when(resourceType.getShortName()).thenReturn("DMN");
+        return resourceType;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected AbstractProjectDiagramEditor createDiagramEditor() {
+        diagramEditor = spy(new DMNDiagramEditor(view,
+                                                 placeManager,
+                                                 errorPopupPresenter,
+                                                 changeTitleNotificationEvent,
+                                                 savePopUpPresenter,
+                                                 (DMNDiagramResourceType) getResourceType(),
+                                                 clientProjectDiagramService,
+                                                 sessionManager,
+                                                 sessionPresenterFactory,
+                                                 sessionCommandFactory,
+                                                 projectMenuItemsBuilder,
+                                                 onDiagramFocusEvent,
+                                                 onDiagramLostFocusEvent,
+                                                 projectMessagesListener,
+                                                 diagramClientErrorHandler,
+                                                 translationService) {
+            {
+                fileMenuBuilder = DMNDiagramEditorTest.this.fileMenuBuilder;
+                workbenchContext = DMNDiagramEditorTest.this.workbenchContext;
+                projectController = DMNDiagramEditorTest.this.projectController;
+                versionRecordManager = DMNDiagramEditorTest.this.versionRecordManager;
+                alertsButtonMenuItemBuilder = DMNDiagramEditorTest.this.alertsButtonMenuItemBuilder;
+                kieView = DMNDiagramEditorTest.this.kieView;
+                overviewWidget = DMNDiagramEditorTest.this.overviewWidget;
+            }
+
+            @Override
+            protected void log(Level level,
+                               String message) {
+                //avoid GWT log initialization.
+            }
+        });
+
+        return diagramEditor;
+    }
+}
