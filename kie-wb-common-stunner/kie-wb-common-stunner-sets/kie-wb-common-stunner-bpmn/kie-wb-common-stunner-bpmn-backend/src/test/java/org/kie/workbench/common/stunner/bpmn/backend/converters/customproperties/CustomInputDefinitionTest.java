f13b1f5de3afbe4ed3ea726b9bc4311d662d83b8
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-06-27 16:29:15 +0200

    fix sytle issues

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/converters/customproperties/CustomInputDefinitionTest.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/converters/customproperties/CustomInputDefinitionTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,7 @@ import org.eclipse.bpmn2.InputSet;
 import org.eclipse.bpmn2.Task;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.kie.workbench.common.stunner.bpmn.backend.converters.fromstunner.Factories.bpmn2;
 
 public class CustomInputDefinitionTest {
@@ -37,11 +37,9 @@ public class CustomInputDefinitionTest {
         StringInput stringInput2 = new StringInput("BLAH2", "ATYPE", "DEFAULTVAL");
         stringInput.of(task).set("VALUE");
 
-
         InputOutputSpecification ioSpecification = task.getIoSpecification();
         List<InputSet> inputSets = ioSpecification.getInputSets();
 
         assertEquals(1, inputSets.size());
     }
-
 }
