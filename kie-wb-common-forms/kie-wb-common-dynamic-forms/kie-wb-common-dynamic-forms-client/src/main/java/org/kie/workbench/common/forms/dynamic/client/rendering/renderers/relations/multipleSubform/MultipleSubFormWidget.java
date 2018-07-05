fc1d98f7ce877704b2c26b202ec92353c8d43831
Pere Fernandez <pere.fernandez@gmail.com>
2018-07-05 16:45:55 +0200

    JBPM-6914: Changes in options are saved on cancel #2: - Deep cloning instances before editing them

--- kie-wb-common-forms/kie-wb-common-dynamic-forms/kie-wb-common-dynamic-forms-client/src/main/java/org/kie/workbench/common/forms/dynamic/client/rendering/renderers/relations/multipleSubform/MultipleSubFormWidget.java
+++ kie-wb-common-forms/kie-wb-common-dynamic-forms/kie-wb-common-dynamic-forms-client/src/main/java/org/kie/workbench/common/forms/dynamic/client/rendering/renderers/relations/multipleSubform/MultipleSubFormWidget.java
@@ -228,8 +228,12 @@ public class MultipleSubFormWidget extends Composite implements TakesValue<List<
                                                   if (values == null) {
                                                       values = new ArrayList<>();
                                                   }
-                                                  values.add(formRenderer.getModel());
-                                                  tableValues.add((HasProperties) formRenderer.getModel());
+
+                                                  BindableProxy proxy = (BindableProxy) formRenderer.getModel();
+
+                                                  values.add(proxy.deepUnwrap());
+                                                  tableValues.add(proxy);
+
                                                   refreshCrud();
                                                   fireFieldChange();
                                                   formRenderer.unBind();
@@ -266,10 +270,8 @@ public class MultipleSubFormWidget extends Composite implements TakesValue<List<
 
                                                   bindingHelper.afterEdit(realProxy);
 
-                                                  values.set(index,
-                                                             realProxy);
-                                                  tableValues.set(index,
-                                                                  realProxy);
+                                                  values.set(index, realProxy.deepUnwrap());
+                                                  tableValues.set(index, realProxy);
 
                                                   refreshCrud();
                                                   fireFieldChange();
