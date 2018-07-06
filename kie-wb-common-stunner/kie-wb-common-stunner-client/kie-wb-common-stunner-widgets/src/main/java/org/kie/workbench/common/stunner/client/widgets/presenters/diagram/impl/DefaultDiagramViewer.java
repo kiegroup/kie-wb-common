apache/incubator-kie-tools

[JPBMP 7384] - Stunner - Add setting for enable/disable HiDPI (#1938)

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/diagram/impl/DefaultDiagramViewer.java
@@ -30,6 +30,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SingleSelection;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistries;
 import org.kie.workbench.common.stunner.core.client.session.impl.InstanceUtils;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -50,6 +51,7 @@ public class DefaultDiagramViewer
     private final ManagedInstance<AbstractCanvasHandler> canvasHandlerInstances;
     private final ManagedInstance<ZoomControl<AbstractCanvas>> zoomControlInstances;
     private final ManagedInstance<SelectionControl<AbstractCanvasHandler, Element>> selectionControlInstances;
+    private final StunnerPreferencesRegistries preferencesRegistries;
 
     private AbstractCanvas canvas;
     private AbstractCanvasHandler canvasHandler;
@@ -62,13 +64,15 @@ public class DefaultDiagramViewer
                                 final @Any ManagedInstance<AbstractCanvasHandler> canvasHandlerInstances,
                                 final @Any ManagedInstance<ZoomControl<AbstractCanvas>> zoomControlInstances,
                                 final @Any @SingleSelection ManagedInstance<SelectionControl<AbstractCanvasHandler, Element>> selectionControlInstances,
-                                final WidgetWrapperView view) {
+                                final WidgetWrapperView view,
+                                final StunnerPreferencesRegistries preferencesRegistries) {
         super(view);
         this.definitionUtils = definitionUtils;
         this.canvasInstances = canvasInstances;
         this.canvasHandlerInstances = canvasHandlerInstances;
         this.zoomControlInstances = zoomControlInstances;
         this.selectionControlInstances = selectionControlInstances;
+        this.preferencesRegistries = preferencesRegistries;
     }
 
     @Override
@@ -140,6 +144,11 @@ public class DefaultDiagramViewer
 
     public AbstractCanvas getCanvas() {
         return canvas;
+    }
+
+    @Override
+    protected StunnerPreferencesRegistries getPreferencesRegistry() {
+        return preferencesRegistries;
     }
 
     @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/diagram/impl/DiagramPreviewProxy.java
@@ -24,6 +24,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.Canvas;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
 import org.kie.workbench.common.stunner.core.client.command.CanvasCommandFactory;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistries;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
 
@@ -37,7 +38,8 @@ public abstract class DiagramPreviewProxy<D extends Diagram> extends AbstractDia
     private final DiagramViewerProxy<D> viewer;
 
     @SuppressWarnings("unchecked")
-    public DiagramPreviewProxy(final WidgetWrapperView view) {
+    public DiagramPreviewProxy(final WidgetWrapperView view,
+                               final StunnerPreferencesRegistries preferencesRegistries) {
         this.viewer =
                 new DiagramViewerProxy<D>(view) {
                     @Override
@@ -58,6 +60,11 @@ public abstract class DiagramPreviewProxy<D extends Diagram> extends AbstractDia
                     @Override
                     protected AbstractCanvas getCanvas() {
                         return DiagramPreviewProxy.this.getCanvas();
+                    }
+
+                    @Override
+                    protected StunnerPreferencesRegistries getPreferencesRegistry() {
+                        return preferencesRegistries;
                     }
 
                     @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/session/impl/SessionEditorImpl.java
@@ -34,6 +34,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.controls.docking.Dock
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
 import org.kie.workbench.common.stunner.core.client.command.CanvasCommandManager;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistries;
 import org.kie.workbench.common.stunner.core.client.session.impl.EditorSession;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -49,12 +50,15 @@ public class SessionEditorImpl<S extends EditorSession>
         implements SessionDiagramEditor<S> {
 
     private final AbstractDiagramViewer<Diagram, AbstractCanvasHandler> diagramViewer;
+    private final StunnerPreferencesRegistries preferencesRegistries;
 
     private Supplier<Diagram> diagramSupplier;
 
     @Inject
-    public SessionEditorImpl(final WidgetWrapperView view) {
+    public SessionEditorImpl(final WidgetWrapperView view,
+                             final StunnerPreferencesRegistries preferencesRegistries) {
         this.diagramViewer = new SessionDiagramEditor(view);
+        this.preferencesRegistries = preferencesRegistries;
         this.diagramSupplier = () -> null != getSessionHandler() ?
                 getSessionHandler().getDiagram() :
                 null;
@@ -156,6 +160,11 @@ public class SessionEditorImpl<S extends EditorSession>
         @Override
         protected AbstractCanvas getCanvas() {
             return getSession().getCanvas();
+        }
+
+        @Override
+        protected StunnerPreferencesRegistries getPreferencesRegistry(){
+            return preferencesRegistries;
         }
 
         @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/session/impl/SessionViewerImpl.java
@@ -29,6 +29,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvas;
 import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistries;
 import org.kie.workbench.common.stunner.core.client.session.impl.ViewerSession;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -44,11 +45,14 @@ public class SessionViewerImpl<S extends ViewerSession>
         implements SessionDiagramViewer<S> {
 
     private final AbstractDiagramViewer<Diagram, AbstractCanvasHandler> diagramViewer;
+    private final StunnerPreferencesRegistries preferencesRegistries;
     private Supplier<Diagram> diagramSupplier;
 
     @Inject
-    public SessionViewerImpl(final WidgetWrapperView view) {
+    public SessionViewerImpl(final WidgetWrapperView view,
+                             final StunnerPreferencesRegistries preferencesRegistries) {
         this.diagramViewer = new SessionDiagramViewer(view);
+        this.preferencesRegistries = preferencesRegistries;
         this.diagramSupplier = () -> null != getSessionHandler() ?
                 getSessionHandler().getDiagram() :
                 null;
@@ -133,6 +137,11 @@ public class SessionViewerImpl<S extends ViewerSession>
         @Override
         protected AbstractCanvas getCanvas() {
             return getSession().getCanvas();
+        }
+
+        @Override
+        protected StunnerPreferencesRegistries getPreferencesRegistry(){
+            return preferencesRegistries;
         }
 
         @Override

kiegroup/kie-wb-common

(cherry picked from commit 764953881bc22d9535fc956396c75ddbeaf59fa2)

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/diagram/impl/DefaultDiagramViewer.java
@@ -30,6 +30,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SingleSelection;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistry;
 import org.kie.workbench.common.stunner.core.client.session.impl.InstanceUtils;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -50,6 +51,7 @@ public class DefaultDiagramViewer
     private final ManagedInstance<AbstractCanvasHandler> canvasHandlerInstances;
     private final ManagedInstance<ZoomControl<AbstractCanvas>> zoomControlInstances;
     private final ManagedInstance<SelectionControl<AbstractCanvasHandler, Element>> selectionControlInstances;
+    private final StunnerPreferencesRegistry preferencesRegistries;
 
     private AbstractCanvas canvas;
     private AbstractCanvasHandler canvasHandler;
@@ -62,13 +64,15 @@ public class DefaultDiagramViewer
                                 final @Any ManagedInstance<AbstractCanvasHandler> canvasHandlerInstances,
                                 final @Any ManagedInstance<ZoomControl<AbstractCanvas>> zoomControlInstances,
                                 final @Any @SingleSelection ManagedInstance<SelectionControl<AbstractCanvasHandler, Element>> selectionControlInstances,
-                                final WidgetWrapperView view) {
+                                final WidgetWrapperView view,
+                                final StunnerPreferencesRegistry preferencesRegistries) {
         super(view);
         this.definitionUtils = definitionUtils;
         this.canvasInstances = canvasInstances;
         this.canvasHandlerInstances = canvasHandlerInstances;
         this.zoomControlInstances = zoomControlInstances;
         this.selectionControlInstances = selectionControlInstances;
+        this.preferencesRegistries = preferencesRegistries;
     }
 
     @Override
@@ -140,6 +144,11 @@ public class DefaultDiagramViewer
 
     public AbstractCanvas getCanvas() {
         return canvas;
+    }
+
+    @Override
+    protected StunnerPreferencesRegistry getPreferencesRegistry() {
+        return preferencesRegistries;
     }
 
     @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/diagram/impl/DiagramPreviewProxy.java
@@ -24,6 +24,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.Canvas;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
 import org.kie.workbench.common.stunner.core.client.command.CanvasCommandFactory;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistry;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
 
@@ -37,7 +38,8 @@ public abstract class DiagramPreviewProxy<D extends Diagram> extends AbstractDia
     private final DiagramViewerProxy<D> viewer;
 
     @SuppressWarnings("unchecked")
-    public DiagramPreviewProxy(final WidgetWrapperView view) {
+    public DiagramPreviewProxy(final WidgetWrapperView view,
+                               final StunnerPreferencesRegistry preferencesRegistries) {
         this.viewer =
                 new DiagramViewerProxy<D>(view) {
                     @Override
@@ -58,6 +60,11 @@ public abstract class DiagramPreviewProxy<D extends Diagram> extends AbstractDia
                     @Override
                     protected AbstractCanvas getCanvas() {
                         return DiagramPreviewProxy.this.getCanvas();
+                    }
+
+                    @Override
+                    protected StunnerPreferencesRegistry getPreferencesRegistry() {
+                        return preferencesRegistries;
                     }
 
                     @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/session/impl/SessionEditorImpl.java
@@ -34,6 +34,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.controls.docking.Dock
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
 import org.kie.workbench.common.stunner.core.client.command.CanvasCommandManager;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistry;
 import org.kie.workbench.common.stunner.core.client.session.impl.EditorSession;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -49,12 +50,15 @@ public class SessionEditorImpl<S extends EditorSession>
         implements SessionDiagramEditor<S> {
 
     private final AbstractDiagramViewer<Diagram, AbstractCanvasHandler> diagramViewer;
+    private final StunnerPreferencesRegistry preferencesRegistries;
 
     private Supplier<Diagram> diagramSupplier;
 
     @Inject
-    public SessionEditorImpl(final WidgetWrapperView view) {
+    public SessionEditorImpl(final WidgetWrapperView view,
+                             final StunnerPreferencesRegistry preferencesRegistries) {
         this.diagramViewer = new SessionDiagramEditor(view);
+        this.preferencesRegistries = preferencesRegistries;
         this.diagramSupplier = () -> null != getSessionHandler() ?
                 getSessionHandler().getDiagram() :
                 null;
@@ -156,6 +160,11 @@ public class SessionEditorImpl<S extends EditorSession>
         @Override
         protected AbstractCanvas getCanvas() {
             return getSession().getCanvas();
+        }
+
+        @Override
+        protected StunnerPreferencesRegistry getPreferencesRegistry() {
+            return preferencesRegistries;
         }
 
         @Override

kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-widgets/src/main/java/org/kie/workbench/common/stunner/client/widgets/presenters/session/impl/SessionViewerImpl.java
@@ -29,6 +29,7 @@ import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvas;
 import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.select.SelectionControl;
 import org.kie.workbench.common.stunner.core.client.canvas.controls.zoom.ZoomControl;
+import org.kie.workbench.common.stunner.core.client.preferences.StunnerPreferencesRegistry;
 import org.kie.workbench.common.stunner.core.client.session.impl.ViewerSession;
 import org.kie.workbench.common.stunner.core.diagram.Diagram;
 import org.kie.workbench.common.stunner.core.graph.Element;
@@ -44,11 +45,14 @@ public class SessionViewerImpl<S extends ViewerSession>
         implements SessionDiagramViewer<S> {
 
     private final AbstractDiagramViewer<Diagram, AbstractCanvasHandler> diagramViewer;
+    private final StunnerPreferencesRegistry preferencesRegistries;
     private Supplier<Diagram> diagramSupplier;
 
     @Inject
-    public SessionViewerImpl(final WidgetWrapperView view) {
+    public SessionViewerImpl(final WidgetWrapperView view,
+                             final StunnerPreferencesRegistry preferencesRegistries) {
         this.diagramViewer = new SessionDiagramViewer(view);
+        this.preferencesRegistries = preferencesRegistries;
         this.diagramSupplier = () -> null != getSessionHandler() ?
                 getSessionHandler().getDiagram() :
                 null;
@@ -133,6 +137,11 @@ public class SessionViewerImpl<S extends ViewerSession>
         @Override
         protected AbstractCanvas getCanvas() {
             return getSession().getCanvas();
+        }
+
+        @Override
+        protected StunnerPreferencesRegistry getPreferencesRegistry() {
+            return preferencesRegistries;
         }
 
         @Override