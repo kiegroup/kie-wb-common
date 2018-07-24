7de450d22e00501f8b70acf2435a9bf9ad6fd3a5
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-07-24 09:49:57 +0200

    Stunner - New Marshallers: Remove println in tests

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/BPMNDirectDiagramMarshallerTest.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/BPMNDirectDiagramMarshallerTest.java
@@ -2669,7 +2669,6 @@ public class BPMNDirectDiagramMarshallerTest {
     public void testMarshallWorkItems() throws Exception {
         Diagram<Graph, Metadata> diagram = unmarshall(BPMN_SERVICE_TASKS);
         String result = tested.marshall(diagram);
-        System.out.println(result);
         assertDiagram(result,
                       1,
                       4,
