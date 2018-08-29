0ce91e7fe16579dad78083a56f920047d0dfb1b1
Massimiliano Dessì <desmax74@yahoo.it>
2018-08-29 21:12:25 +0200

    Removed freeport

--- kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/decorators/JGITCompilerBeforeDecoratorTest.java
+++ kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/decorators/JGITCompilerBeforeDecoratorTest.java
@@ -64,9 +64,6 @@ public class JGITCompilerBeforeDecoratorTest {
 
     @BeforeClass
     public static void setupSystemProperties() {
-        int freePort = TestUtilGit.findFreePort();
-        System.setProperty("org.uberfire.nio.git.daemon.port", String.valueOf(freePort));
-        logger.info("Git port used:{}", freePort);
         //These are not needed for the tests
         System.setProperty("org.uberfire.nio.git.daemon.enabled",
                            "false");
@@ -78,7 +75,7 @@ public class JGITCompilerBeforeDecoratorTest {
 
     @AfterClass
     public static void restoreSystemProperties() {
-        System.clearProperty("org.uberfire.nio.git.daemon.port");
+        System.clearProperty("org.uberfire.sys.repo.monitor.disabled");
         System.clearProperty("org.uberfire.nio.git.daemon.enabled");
         System.clearProperty("org.uberfire.nio.git.ssh.enabled");
     }
