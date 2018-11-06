2a301a9417a4997b14ed26d9a86661f266600bba
tarilabs <matteo.mortari@gmail.com>
2018-11-06 18:10:42 +0100

    DROOLS-3251 [DMN Designer] Question and Allowed answers not marshalled

--- kie-wb-common-dmn/kie-wb-common-dmn-backend/src/main/java/org/kie/workbench/common/dmn/backend/definition/v1_1/DecisionConverter.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-backend/src/main/java/org/kie/workbench/common/dmn/backend/definition/v1_1/DecisionConverter.java
@@ -67,6 +67,8 @@ public class DecisionConverter implements NodeConverter<org.kie.dmn.model.api.De
                                          new BackgroundSet(),
                                          new FontSet(),
                                          new RectangleDimensionsSet());
+        decision.setQuestion(new Question(dmn.getQuestion()));
+        decision.setAllowedAnswers(new AllowedAnswers(dmn.getAllowedAnswers()));
         node.getContent().setDefinition(decision);
 
         if (informationItem != null) {
@@ -96,6 +98,8 @@ public class DecisionConverter implements NodeConverter<org.kie.dmn.model.api.De
             expression.setParent(d);
         }
         d.setExpression(expression);
+        d.setQuestion(source.getQuestion().getValue());
+        d.setAllowedAnswers(source.getAllowedAnswers().getValue());
         // DMN spec table 2: Requirements connection rules
         List<Edge<?, ?>> inEdges = (List<Edge<?, ?>>) node.getInEdges();
         for (Edge<?, ?> e : inEdges) {
