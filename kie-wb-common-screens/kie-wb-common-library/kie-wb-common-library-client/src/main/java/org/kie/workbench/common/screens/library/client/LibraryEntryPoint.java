fe24e7da7044575cada35e293f9e050dff8e8b73
Tiago Bento <tiagobentofernandes@gmail.com>
2019-01-30 13:19:34 -0200

    Expose LibraryPermissions

--- kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-client/src/main/java/org/kie/workbench/common/screens/library/client/LibraryEntryPoint.java
+++ kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-client/src/main/java/org/kie/workbench/common/screens/library/client/LibraryEntryPoint.java
@@ -15,6 +15,8 @@
 
 package org.kie.workbench.common.screens.library.client;
 
+import javax.annotation.PostConstruct;
+
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.jboss.errai.ui.shared.api.annotations.Bundle;
 
@@ -22,4 +24,12 @@ import org.jboss.errai.ui.shared.api.annotations.Bundle;
 @Bundle("resources/i18n/Library.properties")
 public class LibraryEntryPoint {
 
+    @PostConstruct
+    public void init() {
+        exposeCanCreateSpacesPermission();
+    }
+
+    private void exposeCanCreateSpacesPermission() {
+
+    }
 }
