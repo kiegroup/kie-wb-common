5604f768257bf28d81d714589cfc926f9561f30b
Tiago Bento <tiagobentofernandes@gmail.com>
2019-02-19 15:27:10 -0300

    Little improvements

--- kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/healthcheck/HealthCheckService.java
+++ kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/healthcheck/HealthCheckService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
 
 @Path("/")
 @ApplicationScoped
@@ -33,7 +34,7 @@ public class HealthCheckService {
 
     private Response getHealthCheckResponse() {
         return ready
-                ? Response.ok("true").build()
-                : Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("false").build();
+                ? Response.ok(true).build()
+                : Response.status(SERVICE_UNAVAILABLE).entity(false).build();
     }
 }
