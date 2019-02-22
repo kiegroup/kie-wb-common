ab20776d662c997bf022b1c452408c0230450712
William Siqueira <wsiqueir@redhat.com>
2019-02-21 22:22:11 -0300

    JBPM-7654: Adding tests

--- kie-wb-common-forms/kie-wb-common-forms-editor/kie-wb-common-forms-editor-client/src/main/java/org/kie/workbench/common/forms/editor/client/editor/properties/util/DeepCloneHelper.java
+++ kie-wb-common-forms/kie-wb-common-forms-editor/kie-wb-common-forms-editor-client/src/main/java/org/kie/workbench/common/forms/editor/client/editor/properties/util/DeepCloneHelper.java
@@ -29,7 +29,7 @@ public class DeepCloneHelper {
         if(instance == null) {
             return null;
         }
-
+        
         return doDeepClone(instance).deepUnwrap();
     }
 
@@ -65,14 +65,14 @@ public class DeepCloneHelper {
         proxy.set(propertyName, value);
     }
 
-    private static <T> List<Object> doDeepCloneList(List<T> values) {
+    protected static <T> List<Object> doDeepCloneList(List<T> values) {
         return values.stream()
                 .map(DeepCloneHelper::doDeepCloneListValue)
                 .collect(Collectors.toList());
     }
 
     private static <T> Object doDeepCloneListValue(T instance) {
-        if( isProxy(instance.getClass())) {
+        if (isProxy(instance.getClass())) {
             return doDeepClone(instance);
         }
         return instance;
