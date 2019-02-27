27221f89b27d16e8fd13e5b15fcfe6fe989da47a
Guilherme Carreiro <karreiro@gmail.com>
2019-02-27 16:53:28 -0300

    * DROOLS-3669: [DMN Designer] Included Models - Cards View Component
     * Use the generic cards to implement the DMN cards (included models page)

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/included/grid/DMNCardsGridComponent.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.client.editors.included.grid;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import elemental2.dom.HTMLElement;
+import org.jboss.errai.ioc.client.api.ManagedInstance;
+import org.kie.workbench.common.dmn.client.editors.common.cards.CardComponent;
+import org.kie.workbench.common.dmn.client.editors.common.cards.CardsGridComponent;
+import org.kie.workbench.common.dmn.client.editors.included.IncludedModel;
+import org.kie.workbench.common.dmn.client.editors.included.common.IncludedModelsFactory;
+
+public class DMNCardsGridComponent {
+
+    private final ManagedInstance<DMNCardComponent> dmnCardComponent;
+
+    private final CardsGridComponent cardsGridComponent;
+
+    private final IncludedModelsFactory factory;
+
+    @Inject
+    public DMNCardsGridComponent(final ManagedInstance<DMNCardComponent> dmnCardComponent,
+                                 final CardsGridComponent cardsGridComponent,
+                                 final IncludedModelsFactory factory) {
+        this.dmnCardComponent = dmnCardComponent;
+        this.cardsGridComponent = cardsGridComponent;
+        this.factory = factory;
+    }
+
+    public void refresh() {
+        cardsGridComponent.setupCards(dmnCards(makeIncludedModels()));
+    }
+
+    public HTMLElement getElement() {
+        return cardsGridComponent.getElement();
+    }
+
+    private List<CardComponent> dmnCards(final List<IncludedModel> includes) {
+        return includes.stream().map(this::dmnCard).collect(Collectors.toList());
+    }
+
+    private DMNCardComponent dmnCard(final IncludedModel includedModel) {
+        final DMNCardComponent card = dmnCardComponent.get();
+        card.setup(this, includedModel);
+        return card;
+    }
+
+    private List<IncludedModel> makeIncludedModels() {
+        return factory.makeIncludedModels();
+    }
+}
