046d00d98f5d1090b05e6737e83abce0102e563c
Tiago Dolphine <tiagodolphine@gmail.com>
2019-09-18 20:56:01 -0300

    Changing classes package on kie-wb-common-stunner-bpmn-emf to avoid deletion on clean-generated-models profile (#2910)

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-emf/src/main/java/org/eclipse/jbpm/QNameURIHandler.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-emf/src/main/java/org/eclipse/jbpm/QNameURIHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.eclipse.bpmn2.util;
+package org.eclipse.jbpm;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.xmi.resource.xml.URIHandler;
