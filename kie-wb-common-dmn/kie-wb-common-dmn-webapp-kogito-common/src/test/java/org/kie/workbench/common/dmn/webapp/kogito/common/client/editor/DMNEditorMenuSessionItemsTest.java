da7dc9de66bb5df451456227ec65916b1ec1c40e
Michael Anstis <manstis@redhat.com>
2019-10-21 14:02:21 +0100

    KOGITO-405: [DMN Designer] Unit tests: kie-wb-common-dmn-webapp-kogito-common

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/test/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/editor/DMNEditorMenuSessionItemsTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.workbench.common.dmn.webapp.kogito.common.client.editor;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwtmockito.GwtMock;
+import com.google.gwtmockito.GwtMockitoTestRunner;
+import com.google.gwtmockito.WithClassesToStub;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.constants.ButtonSize;
+import org.gwtbootstrap3.client.ui.constants.IconType;
+import org.gwtbootstrap3.client.ui.html.Text;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.workbench.common.dmn.webapp.kogito.common.client.session.DMNEditorSessionCommands;
+import org.kie.workbench.common.stunner.core.client.i18n.ClientTranslationService;
+import org.kie.workbench.common.stunner.core.client.session.command.impl.PerformAutomaticLayoutCommand;
+import org.kie.workbench.common.stunner.core.i18n.CoreTranslationMessages;
+import org.kie.workbench.common.widgets.client.menu.FileMenuBuilder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.uberfire.workbench.model.menu.MenuItem;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(GwtMockitoTestRunner.class)
+@WithClassesToStub({Text.class})
+public class DMNEditorMenuSessionItemsTest {
+
+    @Mock
+    private FileMenuBuilder fileMenuBuilder;
+
+    @Mock
+    private DMNEditorMenuItemsBuilder itemsBuilder;
+
+    @Mock
+    private DMNEditorSessionCommands sessionCommands;
+
+    @Mock
+    private ClientTranslationService translationService;
+
+    @GwtMock
+    private Button button;
+
+    private DMNEditorMenuSessionItems sessionItems;
+
+    @Before
+    public void setup() {
+        this.sessionItems = spy(new DMNEditorMenuSessionItems(itemsBuilder, sessionCommands));
+
+        when(fileMenuBuilder.addNewTopLevelMenu(any(MenuItem.class))).thenReturn(fileMenuBuilder);
+        when(sessionItems.getTranslationService()).thenReturn(translationService);
+        when(translationService.getValue(anyString())).thenAnswer(i -> i.getArguments()[0]);
+    }
+
+    @Test
+    public void testPopulateMenu() {
+        final MenuItem menuItem = mock(MenuItem.class);
+        doNothing().when(sessionItems).superPopulateMenu(any());
+        doReturn(menuItem).when(sessionItems).newPerformAutomaticLayout();
+        sessionItems.populateMenu(fileMenuBuilder);
+
+        verify(sessionItems).addPerformAutomaticLayout(any());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testPerformAutomaticLayoutMenuItem() {
+        final PerformAutomaticLayoutCommand command = mock(PerformAutomaticLayoutCommand.class);
+        final ArgumentCaptor<ClickHandler> clickHandlerArgumentCaptor = ArgumentCaptor.forClass(ClickHandler.class);
+
+        when(sessionCommands.getPerformAutomaticLayoutCommand()).thenReturn(command);
+
+        sessionItems.newPerformAutomaticLayout();
+
+        verify(button).setSize(ButtonSize.SMALL);
+        verify(button).setTitle(CoreTranslationMessages.PERFORM_AUTOMATIC_LAYOUT);
+        verify(button).setIcon(IconType.SITEMAP);
+        verify(button).addClickHandler(clickHandlerArgumentCaptor.capture());
+
+        final ClickHandler clickHandler = clickHandlerArgumentCaptor.getValue();
+        clickHandler.onClick(mock(ClickEvent.class));
+
+        verify(command).execute();
+    }
+
+    @Test
+    public void testEnableMenu() {
+        testMenu(true);
+    }
+
+    @Test
+    public void testDisableMenu() {
+        testMenu(false);
+    }
+
+    private void testMenu(final boolean enabled) {
+        doNothing().when(sessionItems).superSetEnabled(enabled);
+
+        sessionItems.setEnabled(enabled);
+
+        verify(sessionItems).setItemEnabled(PerformAutomaticLayoutCommand.class, enabled);
+    }
+}
