c6497b42fd65282f49412b12e16df1a46a156d21
William Siqueira <wsiqueir@redhat.com>
2019-11-04 15:41:56 -0300

    AF-2354: Create Initial integration MVP with Work Item Handlers

---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/WorkItemDefinitionParser.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.kogito.client.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.guvnor.common.services.project.model.Dependencies;
+import org.kie.workbench.common.stunner.bpmn.workitem.IconDefinition;
+import org.kie.workbench.common.stunner.bpmn.workitem.WorkItemDefinition;
+import org.uberfire.commons.data.Pair;
+
+/**
+ * This is just a POC for a parser. It parses based on line information, so it is easy to break. <br />
+ * Suggestion is to implement a solution character based.
+ *
+ */
+@ApplicationScoped
+public class WorkItemDefinitionParser {
+
+    public List<WorkItemDefinition> parse(String widStr) {
+
+        if (widStr == null || "".equals(widStr.trim())) {
+            return Collections.emptyList();
+        }
+
+        List<WorkItemDefinition> widList = new ArrayList<>();
+        String[] lines = widStr.split("\n");
+        Queue<String> linesQueue = new LinkedList<>(Arrays.asList(lines));
+        while (!linesQueue.isEmpty()) {
+            String line = linesQueue.peek();
+            if (!(empty(line) || isStartingObject(line) || isEndingObject(line))) {
+                WorkItemDefinition wid = parseWorkItemDefinitionObject(linesQueue);
+                widList.add(wid);
+            }
+            linesQueue.poll();
+        }
+        return widList;
+    }
+
+    private boolean empty(String line) {
+        return line == null || line.trim().equals("");
+    }
+
+    private WorkItemDefinition parseWorkItemDefinitionObject(Queue<String> objectQueue) {
+        WorkItemDefinition wid = new WorkItemDefinition();
+        wid.setIconDefinition(new IconDefinition());
+        wid.getIconDefinition().setIconData("");
+        wid.getIconDefinition().setUri("");
+        wid.setUri("");
+        wid.setName("");
+        wid.setCategory("");
+        wid.setDescription("");
+        wid.setDocumentation("");
+        wid.setDisplayName("");
+        wid.setResults("");
+        wid.setDefaultHandler("");
+        wid.setParameters("");
+        wid.setDependencies(new Dependencies());
+        String line = objectQueue.poll();
+        while (!isEndingObject(line) && !objectQueue.isEmpty()) {
+            Pair<String, String> attributes = getAttribute(line);
+            switch (attributes.getK1()) {
+                case "name":
+                    wid.setName(attributes.getK2());
+                    break;
+                case "displayName":
+                    wid.setDisplayName(attributes.getK2());
+                    break;
+                case "icon":
+                    wid.getIconDefinition().setUri(attributes.getK2());
+                    break;
+                case "parameters":
+                    String parameters = "";
+                    if (!isEndingObject(attributes.getK2())) {
+                        parameters = retrieveParameters(objectQueue);
+                    }
+                    wid.setParameters(parameters);
+                    break;
+                case "results":
+                    String results = "";
+                    if (!isEndingObject(attributes.getK2())) {
+                        results = retrieveParameters(objectQueue);
+                    }
+                    wid.setResults(results);
+                    break;
+                default:
+                    break;
+            }
+            line = objectQueue.poll();
+        }
+        return wid;
+    }
+
+    private String retrieveParameters(Queue<String> objectQueue) {
+        String param = objectQueue.poll();
+        StringBuffer sbParams = new StringBuffer(param);
+        while (!(isEndingObject(param) || objectQueue.isEmpty())) {
+            sbParams.append(param);
+            param = objectQueue.poll();
+        }
+        return sbParams.toString();
+    }
+
+    protected Pair<String, String> getAttribute(String value) {
+        Pair<String, String> attrs = new Pair<String, String>("", "");
+        if (value.indexOf(':') != -1) {
+            String[] values = value.split(":");
+            attrs = new Pair<>(cleanProp(values[0]), cleanProp(values[1]));
+        }
+        return attrs;
+    }
+
+    protected String cleanProp(String prop) {
+        return prop.trim().replaceAll("\"", "").replaceAll(",", "");
+    }
+
+    protected boolean isStartingObject(String line) {
+        return line.trim().startsWith("[");
+    }
+
+    protected boolean isEndingObject(String line) {
+        return line == null || line.trim().endsWith("]") || line.trim().endsWith("],");
+    }
+
+}
--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/WorkItemDefinitionStandaloneClientService.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/WorkItemDefinitionStandaloneClientService.java
@@ -17,13 +17,16 @@
 package org.kie.workbench.common.stunner.kogito.client.services;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+import elemental2.dom.DomGlobal;
 import elemental2.promise.Promise;
+import org.appformer.kogito.bridge.client.resource.ResourceContentService;
 import org.kie.workbench.common.stunner.bpmn.client.workitem.WorkItemDefinitionClientService;
 import org.kie.workbench.common.stunner.bpmn.workitem.WorkItemDefinition;
 import org.kie.workbench.common.stunner.bpmn.workitem.WorkItemDefinitionCacheRegistry;
@@ -36,17 +39,40 @@ public class WorkItemDefinitionStandaloneClientService implements WorkItemDefini
 
     private final Promises promises;
     private final WorkItemDefinitionCacheRegistry registry;
+    private final WorkItemDefinitionParser parser;
+    private ResourceContentService resourceContentService;
 
     @Inject
     public WorkItemDefinitionStandaloneClientService(final Promises promises,
-                                                     final WorkItemDefinitionCacheRegistry registry) {
+                                                     final WorkItemDefinitionCacheRegistry registry,
+                                                     final WorkItemDefinitionParser parser,
+                                                     final ResourceContentService resourceContentService) {
         this.promises = promises;
         this.registry = registry;
+        this.parser = parser;
+        this.resourceContentService = resourceContentService;
     }
 
     @Override
     public Promise<Collection<WorkItemDefinition>> call(final Metadata input) {
-        return promises.resolve(registry.items());
+        return promises.create((success, failure) -> {
+            resourceContentService.list("**/*.wid").then(paths -> {
+                if (paths.length > 0) {
+                    String path = paths[0]; // getting first WID found
+                    DomGlobal.console.log("Loading WIDs from " + path);
+                    resourceContentService.get(path).then(value -> {
+                        Collection<WorkItemDefinition> wids = parser.parse(value);
+                        wids.forEach(registry::register);
+                        success.onInvoke(wids);
+                        return null;
+                    });
+                } else {
+                    DomGlobal.console.log("No WIDs to load");
+                    success.onInvoke(Collections.emptyList());
+                }
+                return null;
+            });
+        });
     }
 
     @Produces
