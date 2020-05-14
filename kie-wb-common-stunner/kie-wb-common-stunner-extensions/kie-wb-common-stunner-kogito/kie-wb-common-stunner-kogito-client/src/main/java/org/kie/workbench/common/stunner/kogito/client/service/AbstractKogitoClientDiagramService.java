1589ebc6055d9a8c4e5aca80f9449786cb8bfa45
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-05-14 14:51:20 +0200

    fix another default package

--- kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-kogito/kie-wb-common-stunner-kogito-client/src/main/java/org/kie/workbench/common/stunner/kogito/client/service/AbstractKogitoClientDiagramService.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-kogito/kie-wb-common-stunner-kogito-client/src/main/java/org/kie/workbench/common/stunner/kogito/client/service/AbstractKogitoClientDiagramService.java
@@ -22,7 +22,7 @@ public abstract class AbstractKogitoClientDiagramService implements KogitoClient
 
     private static final char UNIX_SEPARATOR = '/';
     private static final char WINDOWS_SEPARATOR = '\\';
-    public static final String DEFAULT_DIAGRAM_ID = "default";
+    public static final String DEFAULT_DIAGRAM_ID = "com.example";
 
     /**
      * Making correct ID diagram from path:
