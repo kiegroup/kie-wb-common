60668cb5b2bdf3807e6a01dcea4bfef878d26b22
Daniel J dos Santos <djdossantos@live.com>
2020-08-06 11:55:30 -0300

    Changed default behavior.

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/api/ReadOnlyProviderImpl.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/api/ReadOnlyProviderImpl.java
@@ -38,6 +38,6 @@ public class ReadOnlyProviderImpl implements ReadOnlyProvider {
 
     @Override
     public boolean isReadOnlyDiagram() {
-        return contextProvider != null && contextProvider.getChannel() == GITHUB;
+        return false;
     }
 }
