a638021e57209a64af6d87353d292a2a4c70e9ae
Yeser Amer <yamer@redhat.com>
2020-08-13 09:18:53 +0200

    Update kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/main/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/services/PMMLMarshallerService.java
    Co-authored-by: Guilherme Carreiro <karreiro@gmail.com>

--- kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/main/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/services/PMMLMarshallerService.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/main/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/services/PMMLMarshallerService.java
@@ -58,7 +58,7 @@ public class PMMLMarshallerService {
             PMMLDocumentData pmmlDocumentData = pmmlEditorMarshallerApi.getPMMLDocumentData(pmmlFileContent);
             PMMLDocumentMetadata pmmlDocumentMetadata = PMMLMarshallerConverter.fromJSInteropToMetadata(pmmlFilePath, pmmlDocumentData);
             return promises.resolve(pmmlDocumentMetadata);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             return promises.reject("Error during marshalling of PMML file " + pmmlFilePath + ": " + e.getMessage());
         }
     }
