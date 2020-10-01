fb3a00de64465c1c70dd6595e95d1f048796899b
Daniel José dos Santos <djdossantos@live.com>
2020-10-01 18:24:23 -0300

    KOGITO-3364: [DMN Designer] Multiple DRDs support - When a node is deleted in the DRG, undo/redo operations do not work properly

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/session/DMNCanvasHandler.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/session/DMNCanvasHandler.java
@@ -121,5 +121,7 @@ public class DMNCanvasHandler<D extends Diagram, C extends AbstractCanvas> exten
                        final Element child) {
         super.addChild(parent, child);
     }
+
+
 }
 
