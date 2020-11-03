8184785fe68b48daf65ea5555818abece0df4cfd
Daniel José dos Santos <djdossantos@live.com>
2020-11-03 10:21:39 -0300

    Code style

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/test/java/org/kie/workbench/common/dmn/client/docks/navigator/tree/DecisionNavigatorTreeViewTest.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/test/java/org/kie/workbench/common/dmn/client/docks/navigator/tree/DecisionNavigatorTreeViewTest.java
@@ -21,8 +21,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
 
-import javax.enterprise.event.Event;
-
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
