5523e5d7af9053d4f3c794b21e0241891a5a682f
vpellegrino <vpellegr@redhat.com>
2020-11-03 18:23:33 +0100

    Root app

---
+++ kie-wb-common-react/boxed-expression-editor/src/app/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import '@patternfly/react-core/dist/styles/base.css';
+
+const App: React.FunctionComponent = () => (
+  <div>Hello World!</div>
+);
+
+export {App};
