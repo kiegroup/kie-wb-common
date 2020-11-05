6c0e8c425422d854145de0ccedde100484a5433c
vpellegrino <vpellegr@redhat.com>
2020-11-05 17:37:49 +0100

    Unifying to kogito-tooling standards

--- kie-wb-common-react/boxed-expression-editor/prettier.config.js
+++ kie-wb-common-react/boxed-expression-editor/prettier.config.js
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {App} from "./app";
-
-if (process.env.NODE_ENV !== "production") {
-  const config = {
-    rules: [
-      {
-        id: 'color-contrast',
-        enabled: false
-      }
-    ]
-  };
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-  const axe = require("react-axe");
-  axe(React, ReactDOM, 1000, config);
-}
-
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+module.exports = {
+  printWidth: 120
+};
