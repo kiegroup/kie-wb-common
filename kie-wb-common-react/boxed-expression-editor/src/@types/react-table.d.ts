546ae4cc9f1a14bb94e07c930e10ef6374bfe7c4
vpellegrino <vpellegr@redhat.com>
2020-12-17 12:43:11 +0100

    Adding react-table dependencies

---
+++ kie-wb-common-react/boxed-expression-editor/src/@types/react-table.d.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DataType } from "../api";
+import { TableResizerProps } from "react-table";
+
+declare module "react-table" {
+  export interface ColumnInstance {
+    /** Column data type */
+    dataType: DataType;
+    /** When resizable, this function returns the resizer props  */
+    getResizerProps: (props?: Partial<TableResizerProps>) => TableResizerProps;
+    /** It tells whether column can resize or not */
+    canResize: boolean;
+  }
+}
