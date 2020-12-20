b8fba10fce807b12f81b14a953502266d0ef660e
Xiaofeng Bai <bxf12315@gmail.com>
2020-12-20 20:37:45 +0800

    JBPM-9536 After click refresh button, the container will disable

--- kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/main/java/org/kie/workbench/common/screens/server/management/client/container/ContainerPresenter.java
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/main/java/org/kie/workbench/common/screens/server/management/client/container/ContainerPresenter.java
@@ -147,7 +147,9 @@ public class ContainerPresenter {
                 containerSpec != null &&
                 containerSpec.getId() != null &&
                 containerSpec.getId().equals(content.getContainerSpec().getId())) {
-            resetReleaseIdForFailedContainers(content.getContainers(), content.getContainerSpec());
+            if (containerSpec.getStatus() != KieContainerStatus.STOPPED) {
+                resetReleaseIdForFailedContainers(content.getContainers(), content.getContainerSpec());
+            }
             setup(content.getContainerSpec(),
                   content.getContainers());
         } else {
@@ -230,9 +232,17 @@ public class ContainerPresenter {
     }
 
     private boolean isEmpty(final Collection<Container> containers) {
-        for (final Container container : containers) {
-            if (!container.getStatus().equals(KieContainerStatus.STOPPED)) {
-                return false;
+        if (containerSpec.getStatus().equals(KieContainerStatus.STOPPED)) {
+            for (final Container container : containers) {
+                if (!container.getStatus().equals(KieContainerStatus.FAILED) && !container.getStatus().equals(KieContainerStatus.STOPPED)) {
+                    return false;
+                }
+            }
+        } else {
+            for (final Container container : containers) {
+                if (!container.getStatus().equals(KieContainerStatus.STOPPED)) {
+                    return false;
+                }
             }
         }
         return true;
