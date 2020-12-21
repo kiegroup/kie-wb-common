867a8cd5e061db5a4ee392f50d6247bef2c33b00
vpellegrino <vpellegr@redhat.com>
2020-12-21 22:21:52 +0100

    Supporting editable cells + column name/dataType update + Spread of updated relation definition

--- kie-wb-common-react/boxed-expression-editor/src/components/RelationExpression/RelationExpression.tsx
+++ kie-wb-common-react/boxed-expression-editor/src/components/RelationExpression/RelationExpression.tsx
@@ -16,28 +16,54 @@
 
 import "./RelationExpression.css";
 import * as React from "react";
-import { useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import "@patternfly/patternfly/utilities/Text/text.css";
 import { DataType, RelationProps } from "../../api";
-import { Column, useBlockLayout, useResizeColumns, useTable } from "react-table";
+import { Cell, Column, ColumnInstance, Row, useBlockLayout, useResizeColumns, useTable } from "react-table";
 import { TableComposable, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
 import * as _ from "lodash";
 import { EditExpressionMenu } from "../EditExpressionMenu";
 import { useBoxedExpressionEditorI18n } from "../../i18n";
 
-export const RelationExpression: React.FunctionComponent<RelationProps> = ({
-  columns = [{ name: "column-1", dataType: DataType.Undefined }],
-  rows = [[""]],
-}: RelationProps) => {
+function EditableCell({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  onCellUpdate,
+}: {
+  value: string;
+  row: { index: number };
+  column: { id: string };
+  onCellUpdate: (rowIndex: number, columnId: string, value: string) => void;
+}): JSX.Element | string {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onBlur = useCallback(() => {
+    onCellUpdate(index, id, value);
+  }, [id, index, value, onCellUpdate]);
+
+  return <textarea value={value} onChange={onChange} onBlur={onBlur} />;
+}
+
+export const RelationExpression: React.FunctionComponent<RelationProps> = (relationProps: RelationProps) => {
+  const columns =
+    relationProps.columns === undefined
+      ? [{ name: "column-1", label: "column-1", dataType: DataType.Undefined }]
+      : relationProps.columns;
+  const rows = relationProps.rows === undefined ? [{ "column-1": "" }] : relationProps.rows;
   const { i18n } = useBoxedExpressionEditorI18n();
 
   const [tableColumns, setTableColumns] = useState([
-    { Header: "#", accessor: "#", disableResizing: true, width: 60 },
+    { label: "#", accessor: "#", disableResizing: true, width: 60 },
     ..._.map(
       columns,
       (column) =>
         ({
-          Header: column.name,
+          label: column.label,
           accessor: column.name,
           dataType: column.dataType,
         } as Column)
@@ -45,27 +71,53 @@ export const RelationExpression: React.FunctionComponent<RelationProps> = ({
   ]);
 
   const [tableCells, setTableCells] = useState([
-    ..._.map(rows, (row, rowIndex) =>
-      _.reduce(
-        row,
-        (partialRow, cell, columnIndex) => {
-          const columnName = tableColumns[columnIndex + 1].accessor?.toString();
-          if (columnName) {
-            Object.defineProperty(partialRow, columnName, { value: cell });
-          }
-          return partialRow;
-        },
-        { "#": "" + (rowIndex + 1) }
-      )
-    ),
+    ..._.map(rows, (row, rowIndex) => {
+      row["#"] = "" + (rowIndex + 1);
+      return row;
+    }),
   ]);
 
-  console.log(tableCells);
+  useEffect(() => {
+    window.beeApi?.broadcastRelationExpressionDefinition?.({
+      ...relationProps,
+      columns: _.map(tableColumns, (columnInstance: ColumnInstance) => {
+        return {
+          name: columnInstance.accessor,
+          label: columnInstance.label,
+          dataType: columnInstance.dataType,
+        };
+      }),
+      rows: tableCells,
+    });
+  }, [relationProps, tableColumns, tableCells]);
+
+  const onCellUpdate = (rowIndex: number, columnId: string, value: string) => {
+    setTableCells((prevTableCells) => {
+      const updatedTableCells = [...prevTableCells];
+      updatedTableCells[rowIndex][columnId] = value;
+      return updatedTableCells;
+    });
+  };
+
+  const onColumnNameOrDataTypeUpdate = useCallback((columnIndex: number) => {
+    return ({ name = "", dataType = DataType.Undefined }) => {
+      setTableColumns((prevTableColumns: ColumnInstance[]) => {
+        const updatedTableColumns = [...prevTableColumns];
+        updatedTableColumns[columnIndex].label = name;
+        updatedTableColumns[columnIndex].dataType = dataType;
+        return updatedTableColumns;
+      });
+    };
+  }, []);
 
   const tableInstance = useTable(
     {
       columns: tableColumns,
       data: tableCells,
+      defaultColumn: {
+        Cell: (cellRef) => (cellRef.column.canResize ? EditableCell(cellRef) : cellRef.value),
+      },
+      onCellUpdate,
     },
     useBlockLayout,
     useResizeColumns
@@ -76,19 +128,19 @@ export const RelationExpression: React.FunctionComponent<RelationProps> = ({
       <TableComposable variant="compact" {...tableInstance.getTableProps()}>
         <Thead noWrap>
           <tr>
-            {tableInstance.headers.map((column) => {
+            {tableInstance.headers.map((column: ColumnInstance, columnIndex: number) => {
               return (
-                <Th {...column.getHeaderProps()} key={column.id}>
+                <Th {...column.getHeaderProps()} key={columnIndex}>
                   {column.canResize ? (
                     <EditExpressionMenu
                       title={i18n.editRelation}
-                      selectedExpressionName={column.id}
+                      selectedExpressionName={column.label}
                       selectedDataType={column.dataType}
-                      onExpressionUpdate={(expression) => console.log("updated", expression)}
+                      onExpressionUpdate={onColumnNameOrDataTypeUpdate(columnIndex)}
                     >
                       <div className="header-cell">
                         <div>
-                          <p className="pf-u-text-truncate">{column.render("Header")}</p>
+                          <p className="pf-u-text-truncate">{column.label}</p>
                           <p className="pf-u-text-truncate data-type">({column.dataType})</p>
                         </div>
                         <div className="pf-c-drawer" {...column.getResizerProps()}>
@@ -99,7 +151,7 @@ export const RelationExpression: React.FunctionComponent<RelationProps> = ({
                       </div>
                     </EditExpressionMenu>
                   ) : (
-                    <div className="header-cell">{column.render("Header")}</div>
+                    <div className="header-cell">{column.label}</div>
                   )}
                 </Th>
               );
@@ -108,11 +160,11 @@ export const RelationExpression: React.FunctionComponent<RelationProps> = ({
         </Thead>
 
         <Tbody {...tableInstance.getTableBodyProps()}>
-          {tableInstance.rows.map((row, i) => {
+          {tableInstance.rows.map((row: Row, i: number) => {
             tableInstance.prepareRow(row);
             return (
               <Tr {...row.getRowProps()} key={i}>
-                {row.cells.map((cell, j) => {
+                {row.cells.map((cell: Cell, j: number) => {
                   return (
                     <Td {...cell.getCellProps()} key={j}>
                       {cell.render("Cell")}
